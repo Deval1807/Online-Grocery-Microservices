c707b52549ae552d5cb04f98efc50695a60174f0
Deval <deval135darji@gmail.com>
2024-07-09 10:24:42 +0530

    Add test functionality
    - Add sample test functionality using jest

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,13 @@
+// Which service it is
+describe("CustomerService", () => {
+
+    // Which Function
+    describe("SignIn", () => {
+
+        // Which Scenarios we are testing
+        
+        test("validate user inputs", () => {})
+
+        test("Validate response", () => {})
+    })
+})
---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,13 @@
+// Which service it is
+describe("ShoppingService", () => {
+
+    // Which Function
+    describe("PlaceOrder", () => {
+
+        // Which Scenarios we are testing
+        
+        test("validate user inputs", () => {})
+
+        test("Validate response", () => {})
+    })
+})
